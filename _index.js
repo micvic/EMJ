67c4d399b9f8914275e024243d480f90a5b95776
Sevryn <SevBerg129@Gmail.com>
2015-03-01 17:18:23 -0600

    not random anymore

--- _index.js
+++ _index.js
@@ -1,6 +1,11 @@
-﻿$(document).ready(function () {
-    lVal = 1;
-    rVal = 1;
+﻿var lFace = [1, 2, 3, 4];
+var rFace = [3, 4, 1, 2];
+var fi = 0;
+
+
+$(document).ready(function () {
+    var lVal = 1;
+    var rVal = 1;
 
     var scrollPos;
     var changed = 1;
@@ -24,6 +29,8 @@
 
 
 function changeFaces() {
+    fi++;
+    if (fi > 3) { fi -= 4;}
     $("#lFace1").css({ "opacity": "0.0" });
     $("#lFace2").css({ "opacity": "0.0" });
     $("#lFace3").css({ "opacity": "0.0" });
@@ -33,8 +40,10 @@ function changeFaces() {
     $("#rFace3").css({ "opacity": "0.0" });
     $("#rFace4").css({ "opacity": "0.0" });
 
-    lVal = Math.floor(Math.random() * 3) + 1;
+    //lVal = Math.floor(Math.random() * 3) + 1;
     rVal = Math.floor(Math.random() * 3) + 1;
+    lVal = lFace[fi];
+    rVal = rFace[fi];
 
     $("#lFace" + lVal).css({ "opacity": "1.0" });
     $("#rFace" + rVal).css({ "opacity": "1.0" });

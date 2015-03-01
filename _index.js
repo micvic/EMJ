bd3961d7ecc24ed58d48b032a459093bbd26a32f
Sevryn <SevBerg129@Gmail.com>
2015-03-01 17:05:20 -0600

    functioned

--- _index.js
+++ _index.js
@@ -6,59 +6,36 @@
     var changed = 1;
 
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 600 && changed != 2) {
+        if ($(this).scrollTop() > 700 && changed != 2) {
             changed = 2;
-
-            $("#lFace1").css({ "opacity": "0.0" });
-            $("#lFace2").css({ "opacity": "0.0" });
-            $("#lFace3").css({ "opacity": "0.0" });
-            $("#lFace4").css({ "opacity": "0.0" });
-            $("#rFace1").css({ "opacity": "0.0" });
-            $("#rFace2").css({ "opacity": "0.0" });
-            $("#rFace3").css({ "opacity": "0.0" });
-            $("#rFace4").css({ "opacity": "0.0" });
-
-            lVal = Math.floor(Math.random() * 3) + 1;
-            rVal = Math.floor(Math.random() * 3) + 1;
-
-            $("#lFace" + lVal).css({ "opacity": "1.0" });
-            $("#rFace" + rVal).css({ "opacity": "1.0" });
-
+            changeFaces();
         }
 
-        if ($(this).scrollTop() < 600 && changed == 2) {
+        if ($(this).scrollTop() < 700 && changed == 2) {
             changed = 1;
-            $("#lFace1").css({ "opacity": "0.0" });
-            $("#lFace2").css({ "opacity": "0.0" });
-            $("#lFace3").css({ "opacity": "0.0" });
-            $("#lFace4").css({ "opacity": "0.0" });
-            $("#rFace1").css({ "opacity": "0.0" });
-            $("#rFace2").css({ "opacity": "0.0" });
-            $("#rFace3").css({ "opacity": "0.0" });
-            $("#rFace4").css({ "opacity": "0.0" });
-
-            lVal = Math.floor(Math.random() * 3) + 1;
-            rVal = Math.floor(Math.random() * 3) + 1;
-
-            $("#lFace" + lVal).css({ "opacity": "1.0" });
-            $("#rFace" + rVal).css({ "opacity": "1.0" });
+            changeFaces();
         }
     });
 
     $("#dynamicWords").mousedown(function () {
-        $("#lFace1").css({"opacity":"0.0"});
-        $("#lFace2").css({"opacity":"0.0"});
-        $("#lFace3").css({"opacity":"0.0"});
-        $("#lFace4").css({"opacity":"0.0"});
-        $("#rFace1").css({"opacity":"0.0"});
-        $("#rFace2").css({"opacity":"0.0"});
-        $("#rFace3").css({"opacity":"0.0"});
-        $("#rFace4").css({ "opacity": "0.0" });
-
-        lVal = Math.floor(Math.random() * 3) + 1;
-        rVal = Math.floor(Math.random() * 3) + 1;
-
-        $("#lFace"+lVal).css({"opacity":"1.0"});
-        $("#rFace"+rVal).css({"opacity":"1.0"});
+        changeFaces();
     });
 });
+
+
+function changeFaces() {
+    $("#lFace1").css({ "opacity": "0.0" });
+    $("#lFace2").css({ "opacity": "0.0" });
+    $("#lFace3").css({ "opacity": "0.0" });
+    $("#lFace4").css({ "opacity": "0.0" });
+    $("#rFace1").css({ "opacity": "0.0" });
+    $("#rFace2").css({ "opacity": "0.0" });
+    $("#rFace3").css({ "opacity": "0.0" });
+    $("#rFace4").css({ "opacity": "0.0" });
+
+    lVal = Math.floor(Math.random() * 3) + 1;
+    rVal = Math.floor(Math.random() * 3) + 1;
+
+    $("#lFace" + lVal).css({ "opacity": "1.0" });
+    $("#rFace" + rVal).css({ "opacity": "1.0" });
+}
